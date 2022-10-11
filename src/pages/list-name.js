6f33f7258c9a282bf1d5c62c724c1a8f4cc48d39
CharlosDaveee <109518521+ChaaDave@users.noreply.github.com>
2022-10-11 10:49:37 +0700

    Update list-name.js

--- src/pages/list-name.js
+++ src/pages/list-name.js
@@ -56,7 +56,7 @@ export default function ListNamePage() {
     return (
         <div >
             <Container sx={{ my: 18 }} align="center" >
-                <Card sx={{ maxWidth: 500 }} >
+                <Card sx={{ maxWidth: 300 }} >
                     <CardContent >
                         <div><h3>List Nama</h3></div> <br />
 
